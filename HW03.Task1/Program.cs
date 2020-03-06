6462b01ce87a118f35514727869ebea1aff3a9ea
gorlitsa21 <gorlitsa21@gmail.com>
2020-03-07 00:01:16 +0300

    Task 1 is done.

---
+++ HW03.Task1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW03.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 3 примера неявного приведения типов
+
+            byte valueByte = 5;
+            ushort valueUshort = valueByte;
+
+            int valueInt1 = 10;
+            long valueLong1 = valueInt1;
+
+            float valueFloat = 3.14F;
+            double valueDouble = valueFloat;
+
+            // 3 примера явного приведения типов
+
+            long valueLong = 1000;
+            int valueInt = (int)valueLong;
+
+            ushort a = 7;
+            byte b = (byte)a;
+
+            double valueDouble1 = 110.15;
+            decimal valueDec = (decimal)valueDouble1;
+
+            // 3 примера операции упаковки
+
+            int firstValueToBox = 10;
+            object boxedItem1 = firstValueToBox;
+
+            bool secondValueToBox = true;
+            object boxedItem2 = secondValueToBox;
+
+            char thirdValueToBox = 'A';
+            object boxedItem3 = thirdValueToBox;
+
+            // 3 примера операции распаковки
+
+            ushort valueToUnbox1 = (ushort)(int)boxedItem1;
+
+            // Невозможны никакие преобразования из bool в числовые типы и обратно
+            // Согласно справочнику Дж. Албахари
+
+            int valueToUnbox3 = (char)boxedItem3;
+
+        }
+    }
+}
---
+++ HW03.Task1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HW03.Task1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HW03.Task1")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8c944ecf-669e-4cf8-832e-e918b2404350")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
